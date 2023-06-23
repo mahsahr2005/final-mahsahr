0752fc6d0c828588650ed2f403038b74647079a1
mahsahr2005 <127473495+mahsahr2005@users.noreply.github.com>
2023-06-23 17:40:44 +0330

    add manager class for login & signup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
+#include <fstream>
+#include <cstdio>
+class manager {
+public:
+    string username;
+    string password;
+    bool login() {
+        ifstream file;
+        file.open("password.txt",ios::app);
+        if (file.is_open()){
+            string user,pass;
+            while (!file.eof()) {
+                file >> user >> pass;
+                if (this->username==user && this->password == pass) {
+                    return true;
+                }
+            }
+            file.close();
 
+        }
+        return false;
+    }
+    bool signup(){
+       ofstream file;
+       file.open("password.txt",ios::app);
+       if (file.is_open()){
+           file << this->username << endl;
+           file << this->password << endl;
+           return true;
+       }
+       file.close();
+       return false;
+    }
+    bool usernames(){
+        ifstream file;
+        file.open("password.txt");
+        if (file.is_open()){
+            string user;
+            while (!file.eof()){
+                file >> user;
+                if (this->username==user)
+                    return false;
+            }
+        }
+        file.close();
+        return true;
+    }
+};
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
 }
